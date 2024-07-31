e87e0ee0adb1be52bf6522201258398bebc1bca7
sophialynnparedes <74270737+sophialynnparedes@users.noreply.github.com>
2024-07-31 17:06:57 -0500

    installed Sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import "./Styles.css";
+{/*}
 
+User Story #6: On first load, my quote machine displays a random quote in the element with id="text".
 
+User Story #7: On first load, my quote machine displays the random quote's author in the element with id="author".
+
+User Story #8: When the #new-quote button is clicked, my quote machine should fetch a new quote and display it in the #text element.
+
+User Story #9: My quote machine should fetch the new quote's author when the #new-quote button is clicked and display it in the #author element.
+
+User Story #10: I can tweet the current quote by clicking on the #tweet-quote a element. This a element should include the "twitter.com/intent/tweet" path in its href attribute to tweet the current quote.
+
+User Story #11: The #quote-box wrapper element should be horizontally centered. Please run tests with browser's zoom level at 100% and page maximized. */}
 export default function App (props) {
-  return(<></>)
+  return(<>
+    <wrapper id="quote-box">
+      <p id="text">
+        [quote goes here]
+      </p>
+      <p id="author">
+        [author goes here]
+      </p>
+      <button id="new-quote">
+        [Get New Quote]
+      </button>
+      <button id="tweet-quote">
+        [Tweet Quote]
+      </button>
+    </wrapper>
+  </>)
 }
